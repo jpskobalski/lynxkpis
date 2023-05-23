4d0ff56db15cc919559fc31927e3df4258e4fba9
juanskob <juan.skobalski@gmail.com>
2023-05-23 14:52:32 -0300

    modifying chart

--- lynxportal/lynxportal/views.py
+++ lynxportal/lynxportal/views.py
@@ -3,11 +3,27 @@ from django.http import HttpResponse
 from django.template import Template, Context
 from .models import models
 from django.shortcuts import render
+from django.db.models import Max, Sum
 
 def my_view(request):
-    records = models.LynxHistorical.objects.all().order_by('timestamp')
-    timestamps = [record.timestamp for record in records]
-    weights = [record.op_finalweight for record in records]
+    # Get the current year
+    current_year = datetime.datetime.now().year
+    latest_timestamp = models.LynxHistorical.objects.aggregate(Max('timestamp'))['timestamp__max']
+
+    if latest_timestamp is not None:
+        # Filter for records from the latest timestamp and op_closedate year 2023
+        records = models.LynxHistorical.objects.filter(
+            timestamp=latest_timestamp,
+            op_closedate__year=current_year,
+            op_finalweight__isnull=False
+        ).values('op_closedate__month').annotate(total_weight=Sum('op_finalweight')).order_by('op_closedate__month')
+
+        timestamps = [record['op_closedate__month'] for record in records]
+        weights = [record['total_weight'] for record in records]
+    else:
+        timestamps = []
+        weights = []
+
     return render(request, 'template1.html', {'timestamp': timestamps, 'OP_FinalWeight': weights})
     # records = models.LynxHistorical.objects.all()  # or use a filter if you don't want all records
     # return render(request, 'template1.html', {'records': records})
